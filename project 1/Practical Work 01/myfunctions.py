1abf2f8f726a7e690455768d0539b8cf401ba384
Yamroza <oyamroz@gmail.com>
2022-10-04 12:30:05 +0200

    First try on writing functions

--- project 1/Practical Work 01/myfunctions.py
+++ project 1/Practical Work 01/myfunctions.py
@@ -3,11 +3,24 @@
 import numpy as np
 import multiprocessing as mp
 
+
 # Authors of functions:
 # Aleksandra Jamróz
 # Mireia Alba Kesti Izquierdo
 
 
+def tonumpyarray(mp_arr):
+    """
+    Creates a numpy array structure of type unsigned int8, 
+    with the memory used by our global r/w shared memory
+    mp_array - shared memory array with lock
+    """
+    return np.frombuffer(mp_arr.get_obj(),dtype=np.uint8)
+
+
+# FIRST OBLIGATORY FUNCTION FROM INSTRUCTION
+# We have 2 pictures and 2 filters, so we can't make them both global using <<pool_init>>
+# because it would overwrite them in the same memory place later, so using this one will be wrong
 def image_filter(image: np.array, 
                  filter_mask: np.array,
                  numprocesses: int,
@@ -20,9 +33,15 @@ def image_filter(image: np.array,
     numprocesses - number of processes in which the filter must be parallelized
     filtered_image - shared memory variable
     """
+    rows = range(image.shape[0])
+    with mp.Pool(processes = numprocesses,
+                 initializer = pool_init,
+                 initargs = [filtered_image, image, filter_mask]) as p:
+        p.map(edge_filter, rows)
     return 
 
 
+# SECOND OBLIGATORY FUNCTION FROM INSTRUCTION
 def filters_execution(image: np.array,  
                       filter_mask1: np.array, 
                       filter_mask2: np.array,  
@@ -34,7 +53,59 @@ def filters_execution(image: np.array,
     on the same image and saving the result to independent memory spaces. Uses
     previous function for filtering.
     """
+    # doesn't have any inside yet
     return
+
+
+# function copy-pasted from previous session 6
+def pool_init(shared_array_, srcimg, filter_mask):
+    """
+    Initializing the global shared memory data
+    shared_array_ - shared read/write data, with lock. It is a vector 
+    (because the shared memory should be allocated as a vector)
+    srcimg - the original image
+    filtermask - filter which will be applied to the image 
+    Results will be stored in the shared memory array
+    """
+    
+    global shared_space         # define the local process memory reference for shared memory space
+    global shared_matrix        # define the numpy matrix handler
+    global image                # define the readonly memory data as global (the scope of this global variables is the local module)
+    global my_filter
+    
+    # initializing the global read only memory data
+    image = srcimg
+    my_filter = filter_mask
+    size = image.shape
+    shared_space = shared_array_    # assigning the shared memory to the local reference
+    shared_matrix = tonumpyarray(shared_space).reshape(size)    # define the numpy matrix reference to handle data, which will use the shared memory buffer
+
+
+
+def edge_filter(r):
+    
+    global image
+    global my_filter
+    global shared_space
+
+    (rows, cols, depth) = image.shape
+    (filter_rows, filter_cols) = my_filter.shape
+
+    # fetch the row from the original image
+    srow = image[r,:,:]
+
+    # calculating how many rows & cols we have to take
+    previous_rows_number = (filter_rows - 1) / 2
+    previous_cols_number = (filter_cols - 1) / 2    
+
+    for i in range(len(srow)):
+        for j in range(depth):
+            sum = 0             # final pixel value
+            for number in range(previous_rows_number):
+                return
+
+    # THE rest of the function is thinking about if-s coblination how to 
+    # match filters to rows etc.
 
 
 
