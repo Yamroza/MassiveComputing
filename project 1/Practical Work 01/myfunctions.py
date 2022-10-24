04c49b3df91465e59e16ecacdf03b0a1fecb436a
Yamroza <oyamroz@gmail.com>
2022-10-24 17:45:43 +0200

    Deleted pool_init from here

--- project 1/Practical Work 01/myfunctions.py
+++ project 1/Practical Work 01/myfunctions.py
@@ -1,12 +1,13 @@
 # My Functions Library
 
-import numpy as np
-import multiprocessing as mp
-from my_functions2 import filtering
-
 # Authors of functions:
 # Aleksandra Jamróz
 # Mireia Alba Kesti Izquierdo
+
+
+import numpy as np
+import multiprocessing as mp
+import my_functions2 as my2
 
 
 def tonumpyarray(mp_arr):
@@ -16,31 +17,6 @@ def tonumpyarray(mp_arr):
     mp_array - shared memory array with lock
     """
     return np.frombuffer(mp_arr.get_obj(),dtype=np.uint8)
-
-
-def pool_init(shared_array_, srcimg, filter_mask):
-    """
-    Initializing the global shared memory data
-    shared_array_ - shared read/write data, with lock. It is a vector 
-    (because the shared memory should be allocated as a vector)
-    srcimg - the original image
-    filtermask - filter which will be applied to the image 
-    Results will be stored in the shared memory array
-    """
-    
-    global shared_space         # define the local process memory reference for shared memory space
-    global shared_matrix        # define the numpy matrix handler
-    global image                # define the readonly memory data as global (the scope of this global variables is the local module)
-    global my_filter
-    
-    # initializing the global read only memory data
-    image = srcimg
-    my_filter = filter_mask
-    size = image.shape
-    shared_space = shared_array_    # assigning the shared memory to the local reference
-    shared_matrix = tonumpyarray(shared_space).reshape(size)    # define the numpy matrix reference to handle data, which will use the shared memory buffer
-    
-    return 
 
 
 # FIRST OBLIGATORY FUNCTION FROM INSTRUCTION
@@ -58,10 +34,9 @@ def image_filter(image: np.array,
     """
     rows = range(image.shape[0])
     with mp.Pool(processes = numprocesses,
-                 initializer = pool_init,
+                 initializer = my2.pool_init,
                  initargs = [filtered_image, image, filter_mask]) as p:
-        p.map(filtering, rows)
-    return 
+        p.map(my2.filtering, rows)
 
 
 # This cell should be the last one to avoid the execution of 
